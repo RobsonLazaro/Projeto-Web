33c6b450018c44fa8e0f2925ad4d1ab541e09f24
Gabriel Ferreira <gabrielferreira1d2015@gmail.com>
2024-04-07 11:16:58 -0300

    MenuItem update

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import plusPng from '../assets/plus.png'
+import minusPng from '../assets/minus.png'
 
 export default function MenuItem({ id, title, description, price }) {
   return (
@@ -10,9 +12,13 @@ export default function MenuItem({ id, title, description, price }) {
       <div className='flex flex-col gap-2'>
         <span className='text-xl text-orange-400'>R$ {price}</span>
         <div className='flex justify-between'>
-          <button>-</button>
-          <span>0</span>
-          <button>+</button>
+          <button className='flex items-center justify-center w-6 h-6 p-1 text-black bg-white'>
+            <img src={minusPng} alt="Ícone de remover" />
+          </button>
+          <span className='font-semibold'>0</span>
+          <button className='flex items-center justify-center w-6 h-6 p-1 text-black bg-white'>
+            <img src={plusPng} alt="Ícone de adicionar" />
+          </button>
         </div>
       </div>
     </div>
